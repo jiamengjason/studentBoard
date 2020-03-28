b1f96b31033c3fe59cbb976dc2200ffa9084f53c
jiamengmeng <jiamengmeng@100tal.com>
2020-03-28 22:47:05 +0800

    【知名教师】教师详情-全部评论

--- backend/protected/controllers/TeachersController.php
+++ backend/protected/controllers/TeachersController.php
@@ -11,12 +11,12 @@ class TeachersController extends FInterfaceBase
      * 【知名教师】教师详情
      */
     public function actionInfo(){
-        $userId = $this->_gets->getParam('user_id');
-        if (empty($userId)){
+        $teacherId = $this->_gets->getParam('teacher_id');
+        if (empty($teacherId)){
             $this->outputParamsError();
         }
         $usersService = new UsersService();
-        $userInfo = $usersService->getUserInfoByUid($userId);
+        $userInfo = $usersService->getUserInfoByUid($teacherId);
         if (empty($userInfo)){
             $this->outputParamsError();
         }
@@ -26,7 +26,7 @@ class TeachersController extends FInterfaceBase
 
         //查询老师评价得分
         $evaluateScoreService = new EvaluateScoreService();
-        $avgScore = $evaluateScoreService->findAvgScoreByUid($userId);
+        $avgScore = $evaluateScoreService->findAvgScoreByUid($teacherId);
 
         //查询对应机构名称
         $where = 'id = '.$userInfo['parentId'];
@@ -38,7 +38,7 @@ class TeachersController extends FInterfaceBase
 
         //返回数据
         $data = [
-            'user_id' => $userId,
+            'teacher_id' => $teacherId,
             'avgScore' => $avgScore,
             'user_name' => $userInfo['userName'],
             'organization_id' => $userInfo['parentId'],
@@ -54,32 +54,54 @@ class TeachersController extends FInterfaceBase
      * 【知名教师】教师详情-相似老师
      */
     public function actionSimilar(){
-        $userId = $this->_gets->getParam('user_id');
+        $teacherId = $this->_gets->getParam('teacher_id');
         $limit = $this->_gets->getParam('limit');
         $limit = $limit ? $limit : 0;
-        if (empty($userId)){
+        if (empty($teacherId)){
             $this->outputParamsError();
         }
         $usersService = new UsersService();
-        $userInfo = $usersService->getUserInfoByUid($userId);
+        $userInfo = $usersService->getUserInfoByUid($teacherId);
         if (empty($userInfo)){
+            $this->outputParamsError();
+        }
+        if ($userInfo['roleId'] != RoleGroupListConfig::$teacherRoleId){
             $this->outputParamsError();
         }
 
         //查询老师评价得分
         $evaluateScoreService = new EvaluateScoreService();
-        $avgScore = $evaluateScoreService->findAvgScoreByUid($userId);
+        $avgScore = $evaluateScoreService->findAvgScoreByUid($teacherId);
 
         //查询相似的老师评价得分
         $evaluateScoreService = new EvaluateScoreService();
-        $teacherList = $evaluateScoreService->findSimilarTeacherByUid($userId, $avgScore, $limit);
+        $teacherList = $evaluateScoreService->findSimilarTeacherByUid($teacherId, $avgScore, $limit);
 
-        //返回数据
-        $data = [
-            'user_id' => $userId,
-            'user_name' => $userInfo['userName'],
-            'list' => $teacherList
-        ];
+        $this->outputOk('', $teacherList);
+    }
+
+    /**
+     * 【知名教师】教师详情-全部评论
+     */
+    public function actionCommentList(){
+        $params['teacherId'] = $this->_gets->getParam('teacher_id');
+        $params['page'] = $this->_gets->getParam('page');
+        $params['pageSize'] = $this->_gets->getParam('pageSize');
+        if (empty($params['teacherId'])){
+            $this->outputParamsError();
+        }
+        $usersService = new UsersService();
+        $userInfo = $usersService->getUserInfoByUid($params['teacherId']);
+        if (empty($userInfo)){
+            $this->outputParamsError();
+        }
+        if ($userInfo['roleId'] != RoleGroupListConfig::$teacherRoleId){
+            $this->outputParamsError();
+        }
+
+        //查询老师评价得分
+        $evaluateScoreService = new EvaluateScoreService();
+        $data = $evaluateScoreService->findEvaluateScoreListById($params);
 
         $this->outputOk('', $data);
     }
--- backend/protected/service/EvaluateScoreService.php
+++ backend/protected/service/EvaluateScoreService.php
@@ -43,12 +43,12 @@ class EvaluateScoreService
         if (!empty($data)){
             foreach ($data as $item){
                 $teacherList[] = [
-                    'user_id' => $item['evaluated_uid'],
+                    'teacher_id' => $item['evaluated_uid'],
                     'score' => $item['score']
                 ];
             }
             //批量查询老师信息
-            $where = 'id in ('.implode(',', array_column($teacherList, 'user_id')).')';
+            $where = 'id in ('.implode(',', array_column($teacherList, 'teacher_id')).')';
             $userService = new UsersService();
             $userList = $userService->getAllUsersByCondition($where, 'id,role_id,user_name,head_img');
             if (!empty($userList)){
@@ -57,13 +57,52 @@ class EvaluateScoreService
                     $userMap[$userItem['id']] = $userItem;
                 }
                 foreach ($teacherList as $key=>$item){
-                    $teacherList[$key]['role_id'] = isset($userMap[$item['user_id']]) ? $userMap[$item['user_id']]['role_id'] : '';
-                    $teacherList[$key]['user_name'] = isset($userMap[$item['user_id']]) ? $userMap[$item['user_id']]['user_name'] : '';
-                    $teacherList[$key]['head_img'] = isset($userMap[$item['user_id']]) ? $userMap[$item['user_id']]['head_img'] : '';
+                    $teacherList[$key]['role_id'] = isset($userMap[$item['teacher_id']]) ? $userMap[$item['teacher_id']]['role_id'] : '';
+                    $teacherList[$key]['user_name'] = isset($userMap[$item['teacher_id']]) ? $userMap[$item['teacher_id']]['user_name'] : '';
+                    $teacherList[$key]['head_img'] = isset($userMap[$item['teacher_id']]) ? $userMap[$item['teacher_id']]['head_img'] : '';
                 }
             }
         }
 
         return $teacherList;
+    }
+
+    /**
+     * 获取全部评论
+     * @param $params
+     * @return array
+     */
+    public function findEvaluateScoreListById($params){
+        $pageSize = !isset($params['pageSize']) ? CommonEnums::$pageSizeOfAdmin : $params['pageSize'];
+
+        $evaluateScoreModel = new EvaluateScore();
+        $criteria = new CDbCriteria();
+        $criteria->select = 't.id,t.evaluated_uid,t.user_id,t.score,t.give_like,t.give_dislike,t.comment,t.create_time';
+        $criteria->order = 't.create_time desc,t.give_like desc';
+        $criteria->condition = 't.evaluated_uid = ' . $params['teacherId'];
+        $criteria->with = array ('users');
+        $count = $evaluateScoreModel->count($criteria);
+        $pages = new CPagination($count);
+        $pages->pageSize = $pageSize;
+        $criteria->limit = $pages->pageSize;
+        $criteria->offset = $pages->currentPage * $pages->pageSize;
+        $list = $evaluateScoreModel->findAll($criteria);
+
+        $data = [];
+        foreach ($list as $item){
+            $data[] = [
+                'comment_id' => $item['id'],
+                'user_id' => $item['user_id'],
+                'user_name' => $item->users()->user_name,
+                'head_img' => $item->users()->head_img,
+                'score' => $item['score'],
+                'give_like' => $item['give_like'],
+                'give_dislike' => $item['give_dislike'],
+                'create_time' => $item['create_time'],
+                'comment' => $item['comment'],
+            ];
+        }
+
+        return ['list'=>$data, 'pageCount'=>$pages->getPageCount(), 'page'=>$pages->getCurrentPage() + 1, 'pageSize'=>$pages->getPageSize()];
     }
 }
