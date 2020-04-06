a99b983df522dcd75fb9e6b9f77f8b3cf867058d
贾萌培 <jiamengpei.bj@acewill.cn>
2020-04-07 01:24:23 +0800

    列表及详情

--- public/project/src/apis/api_st.js
+++ public/project/src/apis/api_st.js
@@ -2,6 +2,10 @@ import request from "./request";
 const defaultPath = 'index.php?r='
 
 // ----------------------------------机构相关---------------------------------------
+// 获取机构列表
+export const apiGetOrganizationList = params => {
+  return request.get(`${defaultPath}organizations/index&organization_name=${params.organization_name}&page=${params.page}&page_size=${params.page_size}&score_sort=${params.score_sort}`);
+};
 // 获取机构详情
 export const apiGetOrganizationInfo = params => {
   return request.get(`${defaultPath}organizations/detail&organization_id=${params.organization_id}`);
@@ -22,4 +26,19 @@ export const apigetTeachersInfo = params => {
 export const apiGetSimilarTeacher = params => {
   return request.get(`${defaultPath}teachers/similar&teacher_id=${params.teacher_id}`);
 };
+// 获取教师全部评论
+export const apiGetCommentList = params => {
+  return request.get(`${defaultPath}teachers/commentList&teacher_id=${params.teacher_id}`);
+};
 // ----------------------------------教师相关---------------------------------------
+
+// ----------------------------------活动相关---------------------------------------
+// 获取活动列表
+export const apiGetActiveList = params => {
+  return request.get(`${defaultPath}active/index&title=${params.title}&page=${params.page}&page_size=${params.page_size}&type=${params.type}`);
+};
+// 获取活动详细
+export const apiGetActiveInfo = params => {
+  return request.get(`${defaultPath}active/detail&active_id=${params.active_id}`);
+};
+// ----------------------------------活动相关---------------------------------------
