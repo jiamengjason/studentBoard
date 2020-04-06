d14b1733be65aa219b9b93c0f7431553855b6ec8
qiaoyun.zhou <qiaoyun.zhou@zhaopin.com.cn>
2020-04-06 14:48:05 +0800

    feat: 登录接口联调

--- public/project/src/main.js
+++ public/project/src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import "../theme/index.css"
-import { Message } from 'element-ui';
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
@@ -14,8 +13,6 @@ Vue.prototype.$bus = Bus;
 
 // element-ui
 Vue.use(ElementUI);
-Vue.use(Message);
-Vue.prototype.$message = Message;
 
 new Vue({
   router,
